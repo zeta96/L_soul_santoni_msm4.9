e1953c5fdfbb1d04f4741f77dade58a675b4db58
Nich <nctrenco@gmail.com>
2019-05-01 16:01:48 +0000

    msm: camera-v2/santoni: Avoid re-initializing kobjects
    kobject_init gets nasty with the warnings if a previously initialized
    kobject is re-initialized again.

--- drivers/media/platform/msm/camera_v2-legacy/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2-legacy/sensor/msm_sensor_driver.c
@@ -1095,8 +1095,11 @@ int32_t msm_sensor_driver_probe(void *setting,
 	}
 	printk("camera sensor probe %s\n", slave_info->sensor_name);
 
-	rc = kobject_init_and_add(&kobj_back,  &ktype_back, NULL, "camera_fusion_id_back");
-	rc = kobject_init_and_add(&kobj_front,  &ktype_front, NULL, "camera_fusion_id_front");
+	if (!kobj_back.state_initialized)
+		rc = kobject_init_and_add(&kobj_back,  &ktype_back, NULL, "camera_fusion_id_back");
+	if (!kobj_front.state_initialized)
+		rc = kobject_init_and_add(&kobj_front,  &ktype_front, NULL,
+			"camera_fusion_id_front");
 #endif // CONFIG_MACH_XIAOMI_SANTONI
 
 	/* Extract s_ctrl from camera id */
