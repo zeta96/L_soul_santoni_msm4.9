9ee24f302b7f60ca5e6105507af472f481758145
mawrick26 <mawrick26@gmail.com>
2020-05-09 16:51:32 +0200

    drivers: silence IPC Logging failure
    Signed-off-by: mawrick26 <mawrick26@gmail.com>

--- drivers/soc/qcom/glink_smem_native_xprt.c
+++ drivers/soc/qcom/glink_smem_native_xprt.c
@@ -2608,7 +2608,7 @@ static int glink_smem_native_probe(struct platform_device *pdev)
 		einfo->log_ctx =
 			ipc_log_context_create(NUM_LOG_PAGES, log_name, 0);
 	if (!einfo->log_ctx)
-		GLINK_ERR("%s: unable to create log context for [%s:%s]\n",
+		GLINK_DBG("%s: unable to create log context for [%s:%s]\n",
 			__func__, einfo->xprt_cfg.edge,
 			einfo->xprt_cfg.name);
 	register_debugfs_info(einfo);
@@ -2864,7 +2864,7 @@ static int glink_rpm_native_probe(struct platform_device *pdev)
 		einfo->log_ctx =
 			ipc_log_context_create(NUM_LOG_PAGES, log_name, 0);
 	if (!einfo->log_ctx)
-		GLINK_ERR("%s: unable to create log context for [%s:%s]\n",
+		GLINK_DBG("%s: unable to create log context for [%s:%s]\n",
 			__func__, einfo->xprt_cfg.edge,
 			einfo->xprt_cfg.name);
 	register_debugfs_info(einfo);
@@ -3116,7 +3116,7 @@ static int glink_mailbox_probe(struct platform_device *pdev)
 		einfo->log_ctx =
 			ipc_log_context_create(NUM_LOG_PAGES, log_name, 0);
 	if (!einfo->log_ctx)
-		GLINK_ERR("%s: unable to create log context for [%s:%s]\n",
+		GLINK_DBG("%s: unable to create log context for [%s:%s]\n",
 			__func__, einfo->xprt_cfg.edge,
 			einfo->xprt_cfg.name);
 	register_debugfs_info(einfo);
