395ca4a7fda4a69f1ac1385d22a48c56885b3db4
Diab Neiroukh <lazerl0rd@thezest.dev>
2020-07-23 14:53:55 +0100

    power: msm-poweroff: Always force a warm reboot
    This avoids nuking pstore data as we move from the system into recovery.
    
    Suggested-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -57,7 +57,7 @@ static bool scm_deassert_ps_hold_supported;
 static void __iomem *msm_ps_hold;
 static phys_addr_t tcsr_boot_misc_detect;
 static void scm_disable_sdi(void);
-static bool force_warm_reboot;
+static bool force_warm_reboot = true;
 
 #ifdef CONFIG_QCOM_DLOAD_MODE
 /* Runtime could be only changed value once.
