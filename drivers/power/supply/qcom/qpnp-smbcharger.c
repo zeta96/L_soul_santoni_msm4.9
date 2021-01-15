6860adb236e051504c718b023b9571949959fb4c
zeta96 <ahmad.zainul857@gmail.com>
2021-01-15 02:11:20 +0000

    santoni: Reduce thermal mitigation and charging current
    * Stock use 2A

--- drivers/power/supply/qcom/qpnp-smbcharger.c
+++ drivers/power/supply/qcom/qpnp-smbcharger.c
@@ -448,19 +448,19 @@ module_param_named(
 	int, 00600
 );
 
-static int smbchg_default_hvdcp_icl_ma = 2000;
+static int smbchg_default_hvdcp_icl_ma = 1800;
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, 00600
 );
 
-static int smbchg_default_hvdcp3_icl_ma = 3000;
+static int smbchg_default_hvdcp3_icl_ma = 2500;
 module_param_named(
 	default_hvdcp3_icl_ma, smbchg_default_hvdcp3_icl_ma,
 	int, 00600
 );
 
-static int smbchg_default_dcp_icl_ma = 2000;
+static int smbchg_default_dcp_icl_ma = 1800;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, 00600
