73e76d22415952b265d7c4cdbb89de6870fe3fd1
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-19 09:51:43 -0800

    arm64: Move LSE atomics out from the alternatives framework
    We know at compile time whether or not our CPU supports LSE atomics,
    so the alternatives framework is only a hindrance, and can reduce
    performance with unnecessary LL/SC register clobbers. This cleans up the
    LSE atomics to depend solely on CONFIG_ARM64_LSE_ATOMICS for determining
    if these instructions are supported. The ARM64_LSE_ATOMIC_INSN macro is
    now gone, and any remaining prefetch instructions that were intended for
    LL/SC via ARM64_LSE_ATOMIC_INSN are now rolled into the LSE usecase,
    since LSE atomics still benefit from prefetching.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: Ib1f795f83bb36453febc1b51e5a5365f49a905be
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: zeta96 [fix for k4.9]
    
     Conflicts:
    	arch/arm64/include/asm/spinlock.h
    	arch/arm64/kernel/cpufeature.c
    	arch/arm64/lib/Makefile
    	arch/arm64/net/bpf_jit_comp.c

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -882,18 +882,6 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.enable = cpu_enable_pan,
 	},
 #endif /* CONFIG_ARM64_PAN */
-#if defined(CONFIG_AS_LSE) && defined(CONFIG_ARM64_LSE_ATOMICS)
-	{
-		.desc = "LSE atomic instructions",
-		.capability = ARM64_HAS_LSE_ATOMICS,
-		.def_scope = SCOPE_SYSTEM,
-		.matches = has_cpuid_feature,
-		.sys_reg = SYS_ID_AA64ISAR0_EL1,
-		.field_pos = ID_AA64ISAR0_ATOMICS_SHIFT,
-		.sign = FTR_UNSIGNED,
-		.min_field_value = 2,
-	},
-#endif /* CONFIG_AS_LSE && CONFIG_ARM64_LSE_ATOMICS */
 	{
 		.desc = "Software prefetching using PRFM",
 		.capability = ARM64_HAS_NO_HW_PREFETCH,
