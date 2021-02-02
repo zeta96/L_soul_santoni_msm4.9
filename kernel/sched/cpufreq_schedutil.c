8e4f59975ff505022a5284cfdcdf54058ed3df33
zeta96 <ahmad.zainul857@gmail.com>
2021-02-02 17:15:44 +0700

    cpufreq: schedutil: Block init from tampering tunables
    - Only block rate limit up/down without hispeed, and pl
    
    cherry pick from https://github.com/Panchajanya1999/msm-4.14/commits/670fa7abc182c670f339c52fc651188907107414/kernel/sched/cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -14,6 +14,7 @@
 #include <linux/cpufreq.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
+#include <linux/binfmts.h>
 #include <trace/events/power.h>
 #include "sched.h"
 #include "tune.h"
@@ -499,6 +500,10 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	/* Apply init protection, else values will get overwritten */
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -518,6 +523,10 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	/* Apply init protection, else values will get overwritten */
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
