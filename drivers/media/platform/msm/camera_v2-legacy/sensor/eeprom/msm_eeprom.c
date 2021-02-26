Loup-ROM/okami

:sparkles: camera_v2: Included minimal xiaomi's changes

drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
@@ -1575,7 +1575,11 @@ static long msm_eeprom_subdev_fops_ioctl32(struct file *file, unsigned int cmd,
 }
 
 #endif
-
+#if defined(CONFIG_SANTONI_CAMERA)
+static int module_id = -1;
+int main_module_id = -1;
+int sub_module_id = -1;
+#endif // CONFIG_SANTONI_CAMERA
 static int msm_eeprom_platform_probe(struct platform_device *pdev)
 {
 	int rc = 0;
@@ -1724,7 +1728,232 @@ static int msm_eeprom_platform_probe(struct platform_device *pdev)
 		for (j = 0; j < e_ctrl->cal_data.num_data; j++)
 			CDBG("memory_data[%d] = 0x%X\n", j,
 				e_ctrl->cal_data.mapdata[j]);
-
+#if defined(CONFIG_SANTONI_CAMERA)
+		if (!strcmp(eb_info->eeprom_name, "s5k3l8_ofilm")) {
+			CDBG("match id for s5k3l8_ofilm\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for s5k3l8_ofilm module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k3l8_ofilm success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k3l8_ofilm failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k3l8_ofilm_riva")) {
+			CDBG("match id for s5k3l8_ofilm_riva\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for s5k3l8_ofilm_riva module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k3l8_ofilm_riva success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k3l8_ofilm_riva failed\n", __func__);
+				goto power_down;
+			}
+		} else if (!strcmp(eb_info->eeprom_name, "s5k3l8_sunny")) {
+			CDBG("match id for s5k3l8_sunny\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for s5k3l8_sunny module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for s5k3l8_sunny success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k3l8_sunny failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "ov13855_qtech")) {
+			CDBG("match id for ov13855_qtech\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for ov13855_qtech module_id=%d\n", module_id);
+			if (module_id == 11) {
+				CDBG("match id for ov13855_qtech success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov13855_qtech failed\n", __func__);
+				goto power_down;
+			}
+		} else if (!strcmp(eb_info->eeprom_name, "ov13855_sunny")) {
+			CDBG("match id for ov13855_sunny\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for ov13855_sunny module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for ov13855_sunny success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov13855_sunny failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "ov13850")) {
+			CDBG("match id for ov13850\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for ov13850 module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for ov13850 success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov13850 failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k3l2")) {
+			CDBG("match id for s5k3l2\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for s5k3l2 module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k3l2 success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k3l2 failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "ov5675_ofilm")) {
+			CDBG("match id for ov5675_ofilm\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for ov5675_ofilm module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for ov5670_ofilm success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov5670_ofilm failed\n", __func__);
+				goto power_down;
+			}
+		} else if (!strcmp(eb_info->eeprom_name, "s5k5e8_sunny")) {
+			CDBG("match id for s5k5e8_sunny\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for s5k5e8_sunny module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for s5k5e8_sunny success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k5e8_sunny failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k5e8_ofilm_riva")) {
+			CDBG("match id for s5k5e8_ofilm_riva\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for s5k5e8_ofilm_riva module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k5e8_ofilm_riva success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k5e8_ofilm_riva failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "ov5675")) {
+			CDBG("match id for ov5675\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for ov5675 module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for ov5675 success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov5670 failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k5e8_qtech_riva")) {
+			CDBG("match id for s5k5e8_qtech_riva\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for s5k5e8_qtech_riva module_id=%d\n", module_id);
+			if (module_id == 11) {
+				CDBG("match id for s5k5e8_qtech_riva success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k5e8_qtech_riva failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k5e8")) {
+			CDBG("match id for s5k5e8\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for s5k5e8 module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k5e8 success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k5e8 failed\n", __func__);
+				goto power_down;
+			}
+		}  else {
+			pr_err("%s eeprom name match failed\n", __func__);
+			goto power_down;
+		}
+		CDBG("%s eeprom module id: main_module_id=%d  sub_module_id=%d\n", __func__, main_module_id, sub_module_id);
+#endif // CONFIG_SANTONI_CAMERA
 		e_ctrl->is_supported |= msm_eeprom_match_crc(&e_ctrl->cal_data);
 
 		rc = msm_camera_power_down(power_info,

drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
@@ -449,7 +449,9 @@ static int32_t msm_flash_i2c_write_setting_array(
 	}
 	return rc;
 }
-
+#if defined(CONFIG_SANTONI_CAMERA)
+struct msm_flash_ctrl_t *flash_ctrl_wt = NULL;
+#endif // CONFIG_SANTONI_CAMERA
 static int32_t msm_flash_init(
 	struct msm_flash_ctrl_t *flash_ctrl,
 	struct msm_flash_cfg_data_t *flash_data)
@@ -616,6 +618,71 @@ static int32_t msm_flash_low(
 	return 0;
 }
 
+#if defined(CONFIG_SANTONI_CAMERA)
+static int32_t msm_gpio_flash_low(
+		struct msm_flash_ctrl_t *flash_ctrl,
+		struct msm_flash_cfg_data_t *flash_data)
+{
+	CDBG("Enter\n");
+	gpio_direction_output(93, 1);
+	return 0;
+}
+
+static int32_t msm_gpio_flash_high(
+		struct msm_flash_ctrl_t *flash_ctrl,
+		struct msm_flash_cfg_data_t *flash_data)
+{
+	CDBG("Enter\n");
+	gpio_direction_output(90, 1);
+	return 0;
+}
+
+static int32_t msm_gpio_flash_off(
+		struct msm_flash_ctrl_t *flash_ctrl,
+		struct msm_flash_cfg_data_t *flash_data)
+{
+	CDBG("Enter\n");
+	gpio_direction_output(90, 0);
+	gpio_direction_output(93, 0);
+	return 0;
+}
+
+
+int flag_led = 0;
+
+int32_t wt_flash_flashlight(bool boolean)
+{
+	uint32_t curr = 0;
+	int32_t i = 0;
+
+	if (boolean)
+		curr = 100;
+	else
+		curr = 0;
+
+	if (flag_led > 0 && boolean == 0) {
+		return 0;
+	}
+
+	if (flash_ctrl_wt)  {
+		CDBG("WT Enter\n");
+
+		CDBG("WT_XJB  flash_ctrl_wt->torch_num_sources = %d", flash_ctrl_wt->torch_num_sources);
+		for (i = 0; i < flash_ctrl_wt->torch_num_sources - 1; i++) {
+			CDBG("WT low_flash_current[%d] = %d\n", i, curr);
+			if (flash_ctrl_wt->torch_trigger[i]) {
+				led_trigger_event(flash_ctrl_wt->torch_trigger[i],
+						curr);
+			}
+		}
+		if (flash_ctrl_wt->switch_trigger)
+			led_trigger_event(flash_ctrl_wt->switch_trigger, 1);
+		CDBG("WT Exit\n");
+	}
+	return 0;
+}
+#endif // CONFIG_SANTONI_CAMERA
+
 static int32_t msm_flash_high(
 	struct msm_flash_ctrl_t *flash_ctrl,
 	struct msm_flash_cfg_data_t *flash_data)
@@ -677,6 +744,16 @@ static int32_t msm_flash_config(struct msm_flash_ctrl_t *flash_ctrl,
 	mutex_lock(flash_ctrl->flash_mutex);
 
 	CDBG("Enter %s type %d\n", __func__, flash_data->cfg_type);
+
+#if defined(CONFIG_SANTONI_CAMERA)
+	if (flash_data->cfg_type == 2 && flag_led > 0) {
+		flag_led--;
+	} else if (flash_data->cfg_type == 3) {
+		flag_led++;
+	} else if (flash_data->cfg_type == 1) {
+		flag_led = 0;
+	}
+#endif // CONFIG_SANTONI_CAMERA
 
 	switch (flash_data->cfg_type) {
 	case CFG_FLASH_INIT:
@@ -1266,7 +1343,9 @@ static int32_t msm_flash_platform_probe(struct platform_device *pdev)
 
 	if (flash_ctrl->flash_driver_type == FLASH_DRIVER_PMIC)
 		rc = msm_torch_create_classdev(pdev, flash_ctrl);
-
+#if defined(CONFIG_SANTONI_CAMERA)
+	flash_ctrl_wt = flash_ctrl;
+#endif // CONFIG_SANTONI_CAMERA
 	CDBG("probe success\n");
 	return rc;
 }
@@ -1336,9 +1415,15 @@ static struct msm_flash_table msm_gpio_flash_table = {
 	.func_tbl = {
 		.camera_flash_init = msm_flash_gpio_init,
 		.camera_flash_release = msm_flash_release,
+#if defined(CONFIG_SANTONI_CAMERA)
+		.camera_flash_off = msm_gpio_flash_off,
+		.camera_flash_low = msm_gpio_flash_low,
+		.camera_flash_high = msm_gpio_flash_high,
+#else
 		.camera_flash_off = msm_flash_off,
 		.camera_flash_low = msm_flash_low,
 		.camera_flash_high = msm_flash_high,
+#endif // CONFIG_SANTONI_CAMERA
 	},
 };
 

zeta96/L_soul_santoni_msm4.9

drivers/media/platform/msm/camera_v2-legacy/sensor/eeprom/msm_eeprom.c
@@ -1574,6 +1574,12 @@ static long msm_eeprom_subdev_fops_ioctl32(struct file *file, unsigned int cmd,
 
 #endif
 
+#if defined(CONFIG_MACH_XIAOMI_SANTONI)
+static int module_id = -1;
+int main_module_id = -1;
+int sub_module_id = -1;
+#endif // CONFIG_MACH_XIAOMI_SANTONI
+
 static int msm_eeprom_platform_probe(struct platform_device *pdev)
 {
 	int rc = 0;
@@ -1722,7 +1728,232 @@ static int msm_eeprom_platform_probe(struct platform_device *pdev)
 		for (j = 0; j < e_ctrl->cal_data.num_data; j++)
 			CDBG("memory_data[%d] = 0x%X\n", j,
 				e_ctrl->cal_data.mapdata[j]);
-
+#if defined(CONFIG_MACH_XIAOMI_SANTONI)
+		if (!strcmp(eb_info->eeprom_name, "s5k3l8_ofilm")) {
+			CDBG("match id for s5k3l8_ofilm\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for s5k3l8_ofilm module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k3l8_ofilm success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k3l8_ofilm failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k3l8_ofilm_riva")) {
+			CDBG("match id for s5k3l8_ofilm_riva\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for s5k3l8_ofilm_riva module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k3l8_ofilm_riva success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k3l8_ofilm_riva failed\n", __func__);
+				goto power_down;
+			}
+		} else if (!strcmp(eb_info->eeprom_name, "s5k3l8_sunny")) {
+			CDBG("match id for s5k3l8_sunny\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for s5k3l8_sunny module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for s5k3l8_sunny success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k3l8_sunny failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "ov13855_qtech")) {
+			CDBG("match id for ov13855_qtech\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for ov13855_qtech module_id=%d\n", module_id);
+			if (module_id == 11) {
+				CDBG("match id for ov13855_qtech success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov13855_qtech failed\n", __func__);
+				goto power_down;
+			}
+		} else if (!strcmp(eb_info->eeprom_name, "ov13855_sunny")) {
+			CDBG("match id for ov13855_sunny\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for ov13855_sunny module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for ov13855_sunny success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov13855_sunny failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "ov13850")) {
+			CDBG("match id for ov13850\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for ov13850 module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for ov13850 success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov13850 failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k3l2")) {
+			CDBG("match id for s5k3l2\n");
+			if (e_ctrl->cal_data.mapdata[0] == 0x55)  {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			}  else{
+				module_id = -1;
+			}
+			printk("match id for s5k3l2 module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k3l2 success\n");
+				main_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k3l2 failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "ov5675_ofilm")) {
+			CDBG("match id for ov5675_ofilm\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for ov5675_ofilm module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for ov5670_ofilm success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov5670_ofilm failed\n", __func__);
+				goto power_down;
+			}
+		} else if (!strcmp(eb_info->eeprom_name, "s5k5e8_sunny")) {
+			CDBG("match id for s5k5e8_sunny\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for s5k5e8_sunny module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for s5k5e8_sunny success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k5e8_sunny failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k5e8_ofilm_riva")) {
+			CDBG("match id for s5k5e8_ofilm_riva\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for s5k5e8_ofilm_riva module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k5e8_ofilm_riva success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k5e8_ofilm_riva failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "ov5675")) {
+			CDBG("match id for ov5675\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for ov5675 module_id=%d\n", module_id);
+			if (module_id == 1) {
+				CDBG("match id for ov5675 success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for ov5670 failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k5e8_qtech_riva")) {
+			CDBG("match id for s5k5e8_qtech_riva\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for s5k5e8_qtech_riva module_id=%d\n", module_id);
+			if (module_id == 11) {
+				CDBG("match id for s5k5e8_qtech_riva success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k5e8_qtech_riva failed\n", __func__);
+				goto power_down;
+			}
+		}  else if (!strcmp(eb_info->eeprom_name, "s5k5e8")) {
+			CDBG("match id for s5k5e8\n");
+			if (e_ctrl->cal_data.mapdata[16] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[17] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[8] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[9] & 0x1f;
+			} else if (e_ctrl->cal_data.mapdata[0] == 0x55) {
+				module_id = e_ctrl->cal_data.mapdata[1] & 0x1f;
+			} else {
+				module_id = -1;
+			}
+			printk("match id for s5k5e8 module_id=%d\n", module_id);
+			if (module_id == 7) {
+				CDBG("match id for s5k5e8 success\n");
+				sub_module_id = module_id;
+			} else {
+				pr_err("%s match id for s5k5e8 failed\n", __func__);
+				goto power_down;
+			}
+		}  else {
+			pr_err("%s eeprom name match failed\n", __func__);
+			goto power_down;
+		}
+		CDBG("%s eeprom module id: main_module_id=%d  sub_module_id=%d\n", __func__, main_module_id, sub_module_id);
+#endif // CONFIG_MACH_XIAOMI_SANTONI
 		e_ctrl->is_supported |= msm_eeprom_match_crc(&e_ctrl->cal_data);
 
 		rc = msm_camera_power_down(power_info,

drivers/media/platform/msm/camera_v2-legacy/sensor/flash/msm_flash.c
@@ -458,6 +458,10 @@ static int32_t msm_flash_i2c_write_setting_array(
 	return rc;
 }
 
+#if defined(CONFIG_MACH_XIAOMI_SANTONI)
+struct msm_flash_ctrl_t *flash_ctrl_wt = NULL;
+#endif // CONFIG_MACH_XIAOMI_SANTONI
+
 static int32_t msm_flash_init(
 	struct msm_flash_ctrl_t *flash_ctrl,
 	struct msm_flash_cfg_data_t *flash_data)
@@ -624,6 +628,71 @@ static int32_t msm_flash_low(
 	return 0;
 }
 
+#if defined(CONFIG_MACH_XIAOMI_SANTONI)
+static int32_t msm_gpio_flash_low(
+		struct msm_flash_ctrl_t *flash_ctrl,
+		struct msm_flash_cfg_data_t *flash_data)
+{
+	CDBG("Enter\n");
+	gpio_direction_output(93, 1);
+	return 0;
+}
+
+static int32_t msm_gpio_flash_high(
+		struct msm_flash_ctrl_t *flash_ctrl,
+		struct msm_flash_cfg_data_t *flash_data)
+{
+	CDBG("Enter\n");
+	gpio_direction_output(90, 1);
+	return 0;
+}
+
+static int32_t msm_gpio_flash_off(
+		struct msm_flash_ctrl_t *flash_ctrl,
+		struct msm_flash_cfg_data_t *flash_data)
+{
+	CDBG("Enter\n");
+	gpio_direction_output(90, 0);
+	gpio_direction_output(93, 0);
+	return 0;
+}
+
+
+int flag_led = 0;
+
+int32_t wt_flash_flashlight(bool boolean)
+{
+	uint32_t curr = 0;
+	int32_t i = 0;
+
+	if (boolean)
+		curr = 100;
+	else
+		curr = 0;
+
+	if (flag_led > 0 && boolean == 0) {
+		return 0;
+	}
+
+	if (flash_ctrl_wt)  {
+		CDBG("WT Enter\n");
+
+		CDBG("WT_XJB  flash_ctrl_wt->torch_num_sources = %d", flash_ctrl_wt->torch_num_sources);
+		for (i = 0; i < flash_ctrl_wt->torch_num_sources - 1; i++) {
+			CDBG("WT low_flash_current[%d] = %d\n", i, curr);
+			if (flash_ctrl_wt->torch_trigger[i]) {
+				led_trigger_event(flash_ctrl_wt->torch_trigger[i],
+						curr);
+			}
+		}
+		if (flash_ctrl_wt->switch_trigger)
+			led_trigger_event(flash_ctrl_wt->switch_trigger, 1);
+		CDBG("WT Exit\n");
+	}
+	return 0;
+}
+#endif // CONFIG_MACH_XIAOMI_SANTONI
+
 static int32_t msm_flash_high(
 	struct msm_flash_ctrl_t *flash_ctrl,
 	struct msm_flash_cfg_data_t *flash_data)
@@ -685,6 +754,16 @@ static int32_t msm_flash_config(struct msm_flash_ctrl_t *flash_ctrl,
 	mutex_lock(flash_ctrl->flash_mutex);
 
 	CDBG("Enter %s type %d\n", __func__, flash_data->cfg_type);
+
+#if defined(CONFIG_MACH_XIAOMI_SANTONI)
+	if (flash_data->cfg_type == 2 && flag_led > 0) {
+		flag_led--;
+	} else if (flash_data->cfg_type == 3) {
+		flag_led++;
+	} else if (flash_data->cfg_type == 1) {
+		flag_led = 0;
+	}
+#endif // CONFIG_MACH_XIAOMI_SANTONI
 
 	switch (flash_data->cfg_type) {
 	case CFG_FLASH_INIT:
@@ -1272,6 +1351,10 @@ static int32_t msm_flash_platform_probe(struct platform_device *pdev)
 
 	if (flash_ctrl->flash_driver_type == FLASH_DRIVER_PMIC)
 		rc = msm_torch_create_classdev(pdev, flash_ctrl);
+		
+#if defined(CONFIG_MACH_XIAOMI_SANTONI)
+	flash_ctrl_wt = flash_ctrl;
+#endif // CONFIG_MACH_XIAOMI_SANTONI
 
 	CDBG("probe success\n");
 	return rc;
@@ -1342,9 +1425,15 @@ static struct msm_flash_table msm_gpio_flash_table = {
 	.func_tbl = {
 		.camera_flash_init = msm_flash_gpio_init,
 		.camera_flash_release = msm_flash_release,
+#if defined(CONFIG_MACH_XIAOMI_SANTONI)
+		.camera_flash_off = msm_gpio_flash_off,
+		.camera_flash_low = msm_gpio_flash_low,
+		.camera_flash_high = msm_gpio_flash_high,
+#else
 		.camera_flash_off = msm_flash_off,
 		.camera_flash_low = msm_flash_low,
 		.camera_flash_high = msm_flash_high,
+#endif // CONFIG_MACH_XIAOMI_SANTONI
 	},
 };
 