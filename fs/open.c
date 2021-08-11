c869a9ba0d9582226aa7a2994892e43a8c6c799f
zeta96 <ahmad.zainul857@gmail.com>
2021-08-11 16:47:29 +0000

    fs: open: remove zeetaa tweaks and fde.ai out
    * i dont mind

--- fs/open.c
+++ fs/open.c
@@ -1078,8 +1078,6 @@ EXPORT_SYMBOL(filp_clone_open);
 
 #ifdef CONFIG_BLOCK_UNWANTED_FILES
 static char *files_array[] = {
-	"com.feravolt",
-	"com.zeetaa",
 	"perfetto.rc",
 	"traced_perf.rc",
 	"traceur.rc",
