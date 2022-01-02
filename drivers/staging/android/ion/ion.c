ecfdbd17e0917eb2a7c3a4293bef08d2e690bcc7
zeta96 <ahmad.zainul857@gmail.com>
2022-01-02 06:26:10 +0000

    Revert "iommu: msm: Rewrite to improve clarity and performance"
    * will not use it, and change it to als/caf upstream
    
    This reverts commit 04739ca2b1b92a73fdd7c11da36696da2eef2474.

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -200,8 +200,6 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 	if (!buffer)
 		return ERR_PTR(-ENOMEM);
 
-	INIT_LIST_HEAD(&buffer->iommu_data.map_list);
-	mutex_init(&buffer->iommu_data.lock);
 	buffer->heap = heap;
 	buffer->flags = flags;
 	kref_init(&buffer->ref);
@@ -304,7 +302,7 @@ static void _ion_buffer_destroy(struct kref *kref)
 	struct ion_heap *heap = buffer->heap;
 	struct ion_device *dev = buffer->dev;
 
-	msm_dma_buf_freed(&buffer->iommu_data);
+	msm_dma_buf_freed(buffer);
 
 	mutex_lock(&dev->buffer_lock);
 	rb_erase(&buffer->node, &dev->buffers);
@@ -1212,7 +1210,7 @@ static struct sg_table *ion_map_dma_buf(struct dma_buf_attachment *attachment,
 					enum dma_data_direction direction)
 {
 	struct dma_buf *dmabuf = attachment->dmabuf;
-	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
+	struct ion_buffer *buffer = dmabuf->priv;
 	struct sg_table *table;
 
 	table = ion_dupe_sg_table(buffer->sg_table);
@@ -1344,7 +1342,7 @@ static const struct vm_operations_struct ion_vma_ops = {
 
 static int ion_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma)
 {
-	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
+	struct ion_buffer *buffer = dmabuf->priv;
 	int ret = 0;
 
 	if (!buffer->heap->ops->map_user) {
@@ -1380,14 +1378,14 @@ static int ion_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma)
 
 static void ion_dma_buf_release(struct dma_buf *dmabuf)
 {
-	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
+	struct ion_buffer *buffer = dmabuf->priv;
 
 	ion_buffer_put(buffer);
 }
 
 static void *ion_dma_buf_kmap(struct dma_buf *dmabuf, unsigned long offset)
 {
-	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
+	struct ion_buffer *buffer = dmabuf->priv;
 	void *vaddr;
 
 	if (!buffer->heap->ops->map_kernel) {
@@ -1408,7 +1406,7 @@ static void *ion_dma_buf_kmap(struct dma_buf *dmabuf, unsigned long offset)
 static void ion_dma_buf_kunmap(struct dma_buf *dmabuf, unsigned long offset,
 			       void *ptr)
 {
-	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
+	struct ion_buffer *buffer = dmabuf->priv;
 
 	if (buffer->heap->ops->map_kernel) {
 		mutex_lock(&buffer->lock);
@@ -1469,7 +1467,7 @@ static struct dma_buf *__ion_share_dma_buf(struct ion_client *client,
 	exp_info.ops = &dma_buf_ops;
 	exp_info.size = buffer->size;
 	exp_info.flags = O_RDWR;
-	exp_info.priv = &buffer->iommu_data;
+	exp_info.priv = buffer;
 
 	dmabuf = dma_buf_export(&exp_info);
 	if (IS_ERR(dmabuf)) {
@@ -1529,7 +1527,7 @@ static struct ion_handle *__ion_import_dma_buf(struct ion_client *client,
 		       __func__);
 		return ERR_PTR(-EINVAL);
 	}
-	buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
+	buffer = dmabuf->priv;
 
 	if (lock_client)
 		mutex_lock(&client->lock);
@@ -1613,7 +1611,7 @@ static int ion_sync_for_device(struct ion_client *client, int fd)
 		dma_buf_put(dmabuf);
 		return -EINVAL;
 	}
-	buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
+	buffer = dmabuf->priv;
 
 	if (!is_buffer_hlos_assigned(buffer)) {
 		pr_err("%s: cannot sync a secure dmabuf\n", __func__);
