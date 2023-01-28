cdb8ef67edd5ef9e6b6a1a65f88ac63a50bd0a23
Yaroslav Furman <yaro330@gmail.com>
2023-01-29 03:52:13 +0700

    fpc1020_tee: don't fire wakeup when it's active
    Entirely cosmetic change to avoid fingerprint wakeup
    being on top of all others in wakelock stats.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    [pick for our device]

--- drivers/input/fingerprint/fpc/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc/fpc1020_tee.c
@@ -456,7 +456,7 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 	struct fpc1020_data *fpc1020 = handle;
 	dev_dbg(fpc1020->dev, "%s\n", __func__);
 
-	if (atomic_read(&fpc1020->wakeup_enabled)) {
+	if (atomic_read(&fpc1020->wakeup_enabled)&& !fpc1020->ttw_wl.active) {
 		__pm_wakeup_event(&fpc1020->ttw_wl, FPC_TTW_HOLD_TIME);
 	}
 
