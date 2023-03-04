ca5bf00c1401080230e917498f969e5b686fde04
zeta96 <ahmad.zainul857@gmail.com>
2023-03-04 20:05:23 +0000

    Revert "FORWARDPORT: hwmon: qpnp-adc: new vadc_read not breaking suspend"
    - not fixable by this
    
    This reverts commit e8a591b1c15486899d23ff44b3fd02da681fb0e1.

--- drivers/hwmon/qpnp-adc-voltage.c
+++ drivers/hwmon/qpnp-adc-voltage.c
@@ -2031,11 +2031,10 @@ struct qpnp_vadc_chip *qpnp_get_vadc(struct device *dev, const char *name)
 }
 EXPORT_SYMBOL(qpnp_get_vadc);
 
-static int32_t qpnp_vadc_conv_seq_request_base(struct qpnp_vadc_chip *vadc,
-					enum qpnp_vadc_trigger trigger_channel,
+int32_t qpnp_vadc_conv_seq_request(struct qpnp_vadc_chip *vadc,
+				enum qpnp_vadc_trigger trigger_channel,
 					enum qpnp_vadc_channels channel,
-					struct qpnp_vadc_result *result,
-					int pmsafe)
+					struct qpnp_vadc_result *result)
 {
 	int rc = 0, scale_type, amux_prescaling, dt_index = 0, calib_type = 0;
 	uint32_t ref_channel, count = 0, local_idx = 0;
@@ -2276,31 +2275,11 @@ fail_unlock:
 
 	return rc;
 }
-
-int32_t qpnp_vadc_conv_seq_request(struct qpnp_vadc_chip *vadc,
-				enum qpnp_vadc_trigger trigger_channel,
-					enum qpnp_vadc_channels channel,
-					struct qpnp_vadc_result *result)
-{
-	return qpnp_vadc_conv_seq_request_base(vadc, trigger_channel, channel,
-					result, 0);
-}
 EXPORT_SYMBOL(qpnp_vadc_conv_seq_request);
 
-int32_t qpnp_vadc_conv_seq_request_pmsafe(struct qpnp_vadc_chip *vadc,
-					enum qpnp_vadc_trigger trigger_channel,
-					enum qpnp_vadc_channels channel,
-					struct qpnp_vadc_result *result)
-{
-	return qpnp_vadc_conv_seq_request_base(vadc, trigger_channel, channel,
-					result, 1);
-}
-EXPORT_SYMBOL(qpnp_vadc_conv_seq_request_pmsafe);
-
-int32_t qpnp_vadc_read_base(struct qpnp_vadc_chip *vadc,
+int32_t qpnp_vadc_read(struct qpnp_vadc_chip *vadc,
 				enum qpnp_vadc_channels channel,
-				struct qpnp_vadc_result *result,
-				int pmsafe)
+				struct qpnp_vadc_result *result)
 {
 	struct qpnp_vadc_result die_temp_result;
 	int rc = 0;
@@ -2318,16 +2297,15 @@ int32_t qpnp_vadc_read_base(struct qpnp_vadc_chip *vadc,
 	}
 
 	if (channel == VBAT_SNS) {
-		rc = qpnp_vadc_conv_seq_request_base(vadc, ADC_SEQ_NONE,
-						channel, result, pmsafe);
+		rc = qpnp_vadc_conv_seq_request(vadc, ADC_SEQ_NONE,
+				channel, result);
 		if (rc < 0) {
 			pr_err("Error reading vbatt\n");
 			return rc;
 		}
 
-		rc = qpnp_vadc_conv_seq_request_base(vadc, ADC_SEQ_NONE,
-						DIE_TEMP, &die_temp_result,
-						pmsafe);
+		rc = qpnp_vadc_conv_seq_request(vadc, ADC_SEQ_NONE,
+				DIE_TEMP, &die_temp_result);
 		if (rc < 0) {
 			pr_debug("Error reading die_temp\n");
 			return rc;
@@ -2375,25 +2353,10 @@ int32_t qpnp_vadc_read_base(struct qpnp_vadc_chip *vadc,
 
 		return 0;
 	} else
-		return qpnp_vadc_conv_seq_request_base(vadc, ADC_SEQ_NONE,
-						channel, result, pmsafe);
-}
-
-int32_t qpnp_vadc_read(struct qpnp_vadc_chip *vadc,
-				enum qpnp_vadc_channels channel,
-				struct qpnp_vadc_result *result)
-{
-	return qpnp_vadc_read_base(vadc, channel, result, 0);
+		return qpnp_vadc_conv_seq_request(vadc, ADC_SEQ_NONE,
+				channel, result);
 }
 EXPORT_SYMBOL(qpnp_vadc_read);
-
-int32_t qpnp_vadc_read_pmsafe(struct qpnp_vadc_chip *vadc,
-				enum qpnp_vadc_channels channel,
-				struct qpnp_vadc_result *result)
-{
-	return qpnp_vadc_read_base(vadc, channel, result, 1);
-}
-EXPORT_SYMBOL(qpnp_vadc_read_pmsafe);
 
 static void qpnp_vadc_lock(struct qpnp_vadc_chip *vadc)
 {
