2a437cc8f110600dab5b419b9740430e90b36b60
luk1337 <priv.luk@gmail.com>
2023-08-28 13:13:29 +0530

    HACK: binder: Disable freeze related ioctls
    This disables Android's app freezer feature, which just happens to be
    very unstable on Android 13. Incidentally, this is also disabled on
    stock ROM, for unknown reason. Let's hope that we can revert this on
    Android 14 ^.^
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/5384
    Change-Id: I9c9074569807f696fe4b4b5ec77ce235b00dcaaf
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5471,6 +5471,7 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
+#if 0
 	case BINDER_FREEZE: {
 		struct binder_freeze_info info;
 		struct binder_proc **target_procs = NULL, *target_proc;
@@ -5549,6 +5550,7 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
+#endif
 	case BINDER_ENABLE_ONEWAY_SPAM_DETECTION: {
 		uint32_t enable;
 
