84106dd0c4d369f5fd3654ce026296bdaf68b777
zeta96 6 <aster.mojave-ter.aster@tutamail.com>
2024-04-28 13:52:12 +0000

    Revert "sched: Introduce BORE Scheduler (4.2.4)"
    - suggestion, bad on mobile phone
    This reverts commit 59c4b7c8070a90e44b5b86186fb4fe4168826427.

--- kernel/fork.c
+++ kernel/fork.c
@@ -2009,9 +2009,6 @@ static __latent_entropy struct task_struct *copy_process(
 	write_unlock_irq(&tasklist_lock);
 
 	proc_fork_connector(p);
-#ifdef CONFIG_SCHED_BORE
-	sched_post_fork(p);
-#endif // CONFIG_SCHED_BORE
 	cgroup_post_fork(p);
 	threadgroup_change_end(current);
 	perf_event_fork(p);
