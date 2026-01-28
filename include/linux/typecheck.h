660fb6ef11a20c3c66aed8c176ab5537ce269b0d
zeta96 <ahmad.zainul857@gmail.com>
2026-01-28 21:12:20 +0700

    Revert "include/linux/typecheck.h: Zero initialize dummy variables"

--- include/linux/typecheck.h
+++ include/linux/typecheck.h
@@ -6,8 +6,8 @@
  * Always evaluates to 1 so you may use it easily in comparisons.
  */
 #define typecheck(type,x) \
-({	type __dummy = {}; \
-	typeof(x) __dummy2 = {}; \
+({	type __dummy; \
+	typeof(x) __dummy2; \
 	(void)(&__dummy == &__dummy2); \
 	1; \
 })
